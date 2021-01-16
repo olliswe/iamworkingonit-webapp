994aa4cb2929766fbbc006759b6344a89c182e38
olliswe <oliveriyer@gmail.com>
2021-01-16 23:02:22 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./pages/**/*.ts', './components/**/*.ts','./pages/**/*.tsx', './components/**/*.tsx'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}

cadbef041c282f653710b848a5b03ff87ade90b8
olliswe <oliveriyer@gmail.com>
2021-02-04 09:24:40 +0100

    add signup

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,4 +1,4 @@
-import { useLoginMutation } from "apollo/codegen";
+import { useLoginMutation, useSignupMutation } from "apollo/codegen";
 import { useRouter } from "next/router";
 import { TOKEN_PATH } from "config/app";
 import { ROUTES } from "config/routes";
@@ -8,6 +8,7 @@ const useAuth = () => {
   const [loginMutation] = useLoginMutation();
   const router = useRouter();
   const client = useApolloClient();
+  const [signupMutation] = useSignupMutation();
 
   const login = async ({
     email,
@@ -31,13 +32,43 @@ const useAuth = () => {
     }
   };
 
+  const signUp = async ({
+    email,
+    password,
+    firstName,
+    lastName
+  }: {
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+  }) => {
+    console.log("signing up");
+    try {
+      await signupMutation({
+        variables: { email, password, firstName, lastName }
+      });
+      const res = await loginMutation({ variables: { email, password } });
+      const accessToken = res.data?.login.accessToken;
+      if (!accessToken) {
+        return { error: "Unable to signup" };
+      }
+      localStorage.setItem(TOKEN_PATH, accessToken);
+      await router.push(ROUTES.APP_ROOT);
+      return { error: "" };
+    } catch (e) {
+      console.log(e);
+      return { error: e };
+    }
+  };
+
   const logout = async () => {
     localStorage.removeItem(TOKEN_PATH);
     await client.clearStore();
     await router.push(ROUTES.LOGIN);
   };
 
-  return { login, logout };
+  return { login, logout, signUp };
 };
 
 export default useAuth;

d0bf6d0c921d6b7b9357ed3b005351984771f6ec
olliswe <oliveriyer@gmail.com>
2021-02-08 17:09:40 +0100

    loading page

--- components/GlobalLoading.tsx
+++ components/GlobalLoading.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Typical from "react-typical";
 
 const GlobalLoading = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className="w-full h-screen flex justify-center items-center text-7xl font-logo">
+      <div className="w-56">
+        <Typical
+          steps={["ON IT...", 1000, "", 500]}
+          loop={Infinity}
+          wrapper="h1"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default GlobalLoading;
--- global.d.ts
+++ global.d.ts
@@ -2,3 +2,4 @@ declare module "react-animated-term";
 type ReactSetState<T> = React.Dispatch<React.SetStateAction<T>>;
 declare module "deepmerge";
 declare module "react-use-pagination";
+declare module "react-typical";

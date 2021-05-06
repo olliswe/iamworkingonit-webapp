247c92382bc2baa9bc29b70490dcdbc5dd27a6bd
Oliver Iyer <34507469+olliswe@users.noreply.github.com>
2021-05-06 20:51:32 +0200

    fix: config and small stylings

--- components/dashboard/LatestTab.tsx
+++ components/dashboard/LatestTab.tsx
@@ -5,23 +5,25 @@ import UpdateStatusInput from "components/dashboard/UpdateStatusInput";
 
 const LatestTab = ({ team }: { team: TTeam }) => {
   return (
-    <div className="relative min-h-96">
-      <UpdateStatusInput />
-      <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-        {team.users &&
-          team.users.map(user => (
-            <LatestStatusCard
-              user={user}
-              statusUpdate={user.statusupdates && user.statusupdates[0]}
-              key={user.id}
-            />
-          ))}
-      </ul>
-      {team.users?.length === 1 && (
-        <div className="absolute bottom-0 right-2 sm:h-72 h-36">
-          <img src="/images/nowork.svg" alt="me" className="h-full" />
-        </div>
-      )}
+    <div className="h-96">
+      <div className="relative min-h-full">
+        <UpdateStatusInput />
+        <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          {team.users &&
+            team.users.map(user => (
+              <LatestStatusCard
+                user={user}
+                statusUpdate={user.statusupdates && user.statusupdates[0]}
+                key={user.id}
+              />
+            ))}
+        </ul>
+        {team.users?.length === 1 && (
+          <div className="absolute bottom-0 right-2 sm:h-72 h-36">
+            <img src="/images/nowork.svg" alt="me" className="h-full" />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
